99c4e12b9bd655eb14322fe8505a57694989a3bf
chaewon314 <maumsan0623@gmail.com>
2024-01-08 22:19:25 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/*import logo from './logo.svg';*/
+import './App.css';
+
+function App() {
+
+  let post1='블로그1'
+  let post2='블로그2'
+
+  return (
+    <div className="App">
+      <div>
+        
+        <p className='blackfont'>wow!</p>
+      </div>
+      <h4>{post2}</h4>
+    </div>
+  );
+}
+
+export default App;
